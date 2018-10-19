721b124c1ec54d551085fbbecae06deebbeec3e2
yunan.zhang <zyndev@gmail.com>
2018-10-19 09:36:47 +0800

    [+] add zuul

--- common-server/eureka-server/src/main/java/com/zcloud/eurekaserver/EurekaServerApplication.java
+++ common-server/eureka-server/src/main/java/com/zcloud/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,12 @@ package com.zcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author yunan
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
